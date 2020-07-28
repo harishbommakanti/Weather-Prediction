dac67952a7ec39fc4797a5c45819b99968a6392b
Harish <harishbommakanti2002@gmail.com>
2020-07-28 14:21:16 -0500

    added new file for python script

---
+++ script.py
@@ -0,0 +1,18 @@
+#importing modules
+#all purpose stuff
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#ML stuff
+from sklearn import svm
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import confusion_matrix
+#--------------------------------------------------------------
