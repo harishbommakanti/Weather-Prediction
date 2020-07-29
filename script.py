28e08bd6fe82773abbc3364ce724d530641a98d8
Harish <harishbommakanti2002@gmail.com>
2020-07-29 15:37:14 -0500

    added requests library to handle openweather API calls

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 #importing modules
 #all purpose stuff
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd             #for dataframe storage
+import numpy as np              #for math
+import matplotlib.pyplot as plt #for potential graphing
+import requests                 #for openweather API calls at the least
 
 #ML stuff
 from sklearn import svm
@@ -16,3 +17,4 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import confusion_matrix
 #--------------------------------------------------------------
+h1 = requests.get("https://samples.openweathermap.org/data/2.5/history/city?lat=41.85&lon=-87.65&appid=b1b15e88fa797225412429c1c50c122a1") #example API call for openweather
