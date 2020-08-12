9959e0d018395b1157664814ea6984657a59dc1a
Harish <harishbommakanti2002@gmail.com>
2020-08-12 12:53:08 -0500

    added AutoReg model, script is basically done now since predictions look good. need to work on the js logic for API calls and the flask app integration now

--- notebook.py
+++ notebook.py
@@ -2,16 +2,134 @@
 #all purpose stuff
 import pandas as pd             #for dataframe storage
 from pandas import DataFrame
+from pandas import read_csv
 import numpy as np              #for math
 import matplotlib.pyplot as plt #for potential graphing
 import json
+
+#stats and ML stuff
 from scipy.optimize import curve_fit
 from pandas.plotting import autocorrelation_plot
 from sklearn.metrics import mean_squared_error
 
-#ML stuff
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.ar_model import AutoRegResults
+from statsmodels.tsa.ar_model import AutoReg
 #--------------------------------------------------------------
+
+#out of sample AutoReg method
+
+#can create a differenced dataset: value(t) = observed(t) - observed(t-1)
+def difference(data):
+    diff = []
+    for i in range(1,len(data)):
+        value = data[i] - data[i-1]
+        diff.append(value)
+    return np.array(diff)
+
+#make a prediction given regression coefficients and lag observed
+def predict(coef, history):
+    prediction = coef[0]
+    for i in range(1, len(coef)):
+        prediction += coef[i] * history[-i]
+    
+    return prediction
+
+def AutoRegTrain(series):
+    X = difference(series.values)
+
+    #fit the model
+    model = AutoReg(X,lags=6)
+    model_fit = model.fit()
+
+    #save model to file
+    model_fit.save('models/ar_model.pkl')
+    # save the differenced dataset
+    np.save('models/ar_data.npy', X)
+    # save the last ob
+    np.save('models/ar_observation.npy', [series.values[-1]])
+
+def AutoRegPredict():
+    #load the saved model
+    model = AutoRegResults.load('models/ar_model.pkl')
+    data = np.load('models/ar_data.npy')
+    last_observation = np.load('models/ar_observation.npy')
+
+    #make the prediction
+    predictions = model.predict(start=len(data), end=len(data)) #predict the next step out of the sample
+
+    #transform the prediction
+    yhat = predictions[0] + last_observation[0]
+    #print(f'Prediction: {yhat}')
+
+    updateDataSet(yhat)
+
+    return yhat
+
+#after a prediction, update the differeced dataset and the last observation
+def updateDataSet(recentPrediction):
+    observation = recentPrediction
+
+    #load the saved data
+    data = np.load('models/ar_data.npy')
+    last_observation = np.load('models/ar_observation.npy')
+
+    #update and save the differenced observation
+    diffed = observation - last_observation[0]
+    data = np.append(data, [diffed], axis=0)
+    np.save('models/ar_data.npy', data)
+
+    #update and save the real observation
+    last_observation[0] = observation
+    np.save('models/ar_observation.npy', last_observation)
+
+
+
+def make_predictions():
+    """Trains the AutoReg model and returns an array of predictions"""
+    #predict the future
+
+    numTimeSteps = 16
+    X_predict = np.linspace(40, 40+numTimeSteps-1, num=numTimeSteps)
+    predictions = []
+
+    #load the dataset
+    series = read_csv('test_dataupload.csv', header=0, index_col=0, squeeze=True)
+    #print(series.shape)
+
+    for i in range(numTimeSteps):
+        AutoRegTrain(series) #train based on past data and differenced data
+        newPrediction = AutoRegPredict()
+        predictions.append(newPrediction) #add new prediction to predictions array
+
+        #update series by adding the new prediction to it, treating it as ground truth data
+        series = series.append(pd.Series([newPrediction], index=[40+i]))
+
+    #plot the data to see how feasible it is, seems very feasible
+
+    #print(series)
+    series.plot()
+    plt.plot(X_predict, predictions, color='red')
+    plt.show()
+
+
+    return predictions
+
+make_predictions()
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------
+#other methods that didn't work out as well as I would've thought
 
 def inSampleARIMA():
     df = pd.read_csv('test_dataupload.csv', header=0, index_col=0, squeeze=True)
